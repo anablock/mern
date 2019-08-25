ae189ec567ff335bf659516c19fe7f08b6bae58a
Vuk <vuchina@icloud.com>
2019-08-25 16:51:56 -0700

    Version 1.0.4

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`Server Started on ${port}`));
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server Started on ${port}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
