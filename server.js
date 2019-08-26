5ff344b8dc914c0a36117cf3ba7a295468f9c3cf
Vuk <vuchina@icloud.com>
2019-08-25 17:32:37 -0700

    add

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 //const bodyParser = require("body-parser");
 const path = require("path");
 //const config = require('config');
-const config = require('./node_modules/config');
+//const config = require('./node_modules/config');
+config = require('../config/config');
 
 //const items = require('./routes/api/items');
 
