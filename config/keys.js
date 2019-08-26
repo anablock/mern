30857e9d3d9dbc9b42dde0c2a08173baa788590d
Vuk <vuchina@icloud.com>
2019-08-25 22:10:15 -0700

    add keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://anablock_vuk:Beba2019@ds021326.mlab.com:21326/mern_anablock"
+    "mongodb://db_user:Beba2019@ds213348-a0.mlab.com:13348,ds213348-a1.mlab.com:13348/anablock_webapp?replicaSet=rs-ds213348"
 };
 
